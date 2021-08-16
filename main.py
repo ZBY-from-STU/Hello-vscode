e8b3e784896378263b0d9af24e9b4e8e2c4a0246
ZBY from STU <19byzheng@stu.edu.cn>
2021-08-17 03:42:50 +0800

    Add some information in main.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ now_time = datetime.datetime.now()
 
 if __name__ == "__main__":
     print(f"Welcome to meet Vscode, it's {now_time.strftime('%Y年%m月%d日 %H时%M分%S秒')}")
+    print("Here can put more information")
